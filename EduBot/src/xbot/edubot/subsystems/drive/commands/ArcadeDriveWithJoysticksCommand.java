74b19ee062093c64fca31988486a984a42b943a9
Gabriel Young <peregrineautumn@gmail.com>
2018-01-09 19:41:14 -0800

    Arcade drive done easy (power over proportion)

--- EduBot/src/xbot/edubot/subsystems/drive/commands/ArcadeDriveWithJoysticksCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/ArcadeDriveWithJoysticksCommand.java
@@ -2,14 +2,26 @@ package xbot.edubot.subsystems.drive.commands;
 
 import com.google.inject.Inject;
 
+import edu.wpi.first.wpilibj.MockDistanceSensor;
 import xbot.common.command.BaseCommand;
+import xbot.common.controls.actuators.XSpeedController;
+import xbot.edubot.operator_interface.OperatorInterface;
+import xbot.edubot.rotation.MockHeadingSensor;
 import xbot.edubot.subsystems.drive.DriveSubsystem;
 
 public class ArcadeDriveWithJoysticksCommand extends BaseCommand {
 
+	DriveSubsystem drive;
+	OperatorInterface operate;
+
+	
 	@Inject
-	public ArcadeDriveWithJoysticksCommand(DriveSubsystem driveSubsystem) {
+	public ArcadeDriveWithJoysticksCommand(DriveSubsystem driveSubsystem, OperatorInterface oi) {
 		// TODO Auto-generated constructor stub
+		drive = driveSubsystem;
+		operate = oi;
+		
+		
 	}
 	
 	@Override
@@ -21,7 +33,11 @@ public class ArcadeDriveWithJoysticksCommand extends BaseCommand {
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		
+		double X = operate.leftJoystick.getVector().x;
+		double Y = operate.leftJoystick.getVector().y;
+		double L = Y+X;
+		double R = Y-X;
+		drive.tankDrive(L,R);
 	}
 
 }
